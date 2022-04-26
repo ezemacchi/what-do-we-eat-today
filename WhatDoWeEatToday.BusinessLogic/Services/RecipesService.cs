32d3d81b0a4e75ec7c167a9b9b9871925207802c
Ezequiel Macchi <macchimallo@gmail.com>
2022-04-26 17:11:37 -0300

    partial implementation of recipeService, getRecipesWithIngredient

--- WhatDoWeEatToday.BusinessLogic/Services/RecipesService.cs
+++ WhatDoWeEatToday.BusinessLogic/Services/RecipesService.cs
@@ -1,13 +1,25 @@
 ﻿using WhatDoWeEatToday.BusinessLogic.Dtos;
 using WhatDoWeEatToday.BusinessLogic.Services.Contracts;
+using WhatDoWeEatToday.DataAcess.Repositories.Contracts;
 
 namespace WhatDoWeEatToday.BusinessLogic.Services
 {
     public class RecipesService : IRecipesService
     {
+        private readonly IRecipesRepository _recipesRepository;
+
+        public RecipesService(IRecipesRepository recipesRepository)
+        {
+            _recipesRepository = recipesRepository;
+        }
+
         public List<RecipeDto> GetRecipesWithIngredient(string ingredient)
         {
-            return new List<RecipeDto>();
+            var recipes = _recipesRepository.GetRecipesWithIngredient(ingredient);
+
+            var mappedRecipes = new RecipeDto();
+
+            return mappedRecipes;
         }
     }
 }
